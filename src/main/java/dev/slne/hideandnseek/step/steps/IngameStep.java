8f6b8bce42b4e3530a8cb066e090e2ef5957a7b3
twisti <denizgebauer@web.de>
2024-09-06 21:59:21 +0200

    refactor: added todo

--- src/main/java/dev/slne/hideandnseek/step/steps/IngameStep.java
+++ src/main/java/dev/slne/hideandnseek/step/steps/IngameStep.java
@@ -1,14 +1,14 @@
 package dev.slne.hideandnseek.step.steps;
 
+import dev.slne.hideandnseek.GameData;
 import dev.slne.hideandnseek.HideAndSeekEndReason;
 import dev.slne.hideandnseek.HideAndSeekGame;
 import dev.slne.hideandnseek.HideAndSeekGameState;
 import dev.slne.hideandnseek.Messages;
 import dev.slne.hideandnseek.player.HideAndSeekPlayer;
 import dev.slne.hideandnseek.step.GameStep;
-import dev.slne.hideandnseek.util.Continuation;
-import dev.slne.hideandnseek.GameData;
 import dev.slne.hideandnseek.timer.GameCountdown;
+import dev.slne.hideandnseek.util.Continuation;
 import java.time.Duration;
 import net.kyori.adventure.sound.Sound;
 import net.kyori.adventure.sound.Sound.Emitter;
@@ -49,7 +49,7 @@ public class IngameStep extends GameStep {
 
   @Override
   public void start(Continuation continuation) {
-    TextComponent.Builder builder = Component.text();
+    TextComponent.Builder builder = Component.text(); // TODO: 06.09.2024 21:58 - extract to HiderPreparationCountdown#onEnd() ?
 
     builder.append(Messages.prefix()).appendNewline();
     builder.append(Component.text("-".repeat(20))).appendNewline();
@@ -65,9 +65,12 @@ public class IngameStep extends GameStep {
 
     Bukkit.broadcast(builder.build());
 
-    Bukkit.getServer().playSound(
-        Sound.sound().type(org.bukkit.Sound.ENTITY_ENDER_DRAGON_GROWL).volume(.75f).pitch(.75f)
-            .source(Source.MASTER).build(), Emitter.self());
+    Bukkit.getServer().playSound(Sound.sound()
+        .type(org.bukkit.Sound.ENTITY_ENDER_DRAGON_GROWL)
+        .volume(.75f)
+        .pitch(.75f)
+        .source(Source.MASTER)
+        .build(), Emitter.self());
 
     for (HideAndSeekPlayer seeker : game.getSeekers()) {
       seeker.prepareForGame();
