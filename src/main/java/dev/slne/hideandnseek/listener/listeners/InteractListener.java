e139a99a7835202d9df0a6c64dc3a4cd6ff329b9
Red <red.dev@hexle.at>
2024-10-28 20:16:49 +0100

    fix(hide-and-seek): player can now interact with doors

--- src/main/java/dev/slne/hideandnseek/listener/listeners/InteractListener.java
+++ src/main/java/dev/slne/hideandnseek/listener/listeners/InteractListener.java
@@ -1,6 +1,7 @@
 package dev.slne.hideandnseek.listener.listeners;
 
 import dev.slne.hideandnseek.HideAndSeekManager;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -20,8 +21,27 @@ public class InteractListener implements Listener {
   public void onInteract(PlayerInteractEvent event) {
     Block clickedBlock = event.getClickedBlock();
 
-    if (clickedBlock != null && !HideAndSeekManager.INSTANCE.isBypassing(event.getPlayer())) {
+    if(clickedBlock == null){
+      return;
+    }
+
+    if(this.isDoor(clickedBlock.getType())){
+      return;
+    }
+
+    if (!HideAndSeekManager.INSTANCE.isBypassing(event.getPlayer())) {
       event.setCancelled(true);
     }
+  }
+
+  private boolean isDoor(Material material) {
+    return material == Material.OAK_DOOR ||
+        material == Material.SPRUCE_DOOR ||
+        material == Material.BIRCH_DOOR ||
+        material == Material.JUNGLE_DOOR ||
+        material == Material.ACACIA_DOOR ||
+        material == Material.DARK_OAK_DOOR ||
+        material == Material.CRIMSON_DOOR ||
+        material == Material.WARPED_DOOR;
   }
 }
